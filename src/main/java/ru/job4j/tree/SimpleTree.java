aea4c205d932e6e103b3f85589ceb0ed529b7bab
edgar <edgar_dallakyan@mail.ru>
2024-05-07 17:24:24 +0300

    2. Добавить метод boolean isBinary() [#1712]

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,33 +1,43 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
-
     public SimpleTree(final E root) {
         this.root = new Node<>(root);
     }
-
     @Override
     public boolean add(E parent, E child) {
-        Optional<Node<E>> parentNode = findBy(parent);
-        Optional<Node<E>> childNode = findBy(child);
-        boolean result = parentNode.isPresent() && childNode.isEmpty();
-        if (result) {
-            parentNode.get().children.add(new Node<>(child));
+        boolean result = false;
+        Optional<Node<E>> parentOptional = findBy(parent);
+        Optional<Node<E>> childOptional = findBy(child);
+        if (parentOptional.isPresent() && childOptional.isEmpty()) {
+            parentOptional.get().children.add(new Node<>(child));
+            result = true;
         }
         return result;
     }
 
+    public boolean isBinary() {
+        Predicate<Node<E>> isBinaryCondition = element -> element.children.size() > 2;
+        return findByPredicate(isBinaryCondition).isEmpty();
+    }
+
     @Override
     public Optional<Node<E>> findBy(E value) {
+        Predicate<Node<E>> findByCondition = element -> element.value.equals(value);
+        return findByPredicate(findByCondition);
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> result = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> element = data.poll();
-            if (element.value.equals(value)) {
+            if (condition.test(element)) {
                 result = Optional.of(element);
                 break;
             }
