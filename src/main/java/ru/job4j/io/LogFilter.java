a8ecebf63631d21a7279dd8178ddf2c8c35b6f49
edgar <edgar_dallakyan@mail.ru>
2024-05-26 15:58:01 +0300

    5.1. BufferedReader [#252489 #497975]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -23,19 +23,6 @@ public class LogFilter {
         }
         return rsl;
     }
-    public void saveTo(String out) {
-        var data = filter();
-        try (PrintWriter output = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(out)
-                ))) {
-            for (String str : data) {
-                output.println(str);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter("data/log.txt");
