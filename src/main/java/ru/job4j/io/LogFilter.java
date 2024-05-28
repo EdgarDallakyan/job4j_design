06424260ff029e67052726b88cbce2e8f5ae93b2
edgar <edgar_dallakyan@mail.ru>
2024-05-28 15:27:50 +0300

    5.2. BufferedOutputStream [#252490]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -37,7 +37,6 @@ public class LogFilter {
         }
     }
 
-
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter("data/log.txt");
         logFilter.filter().forEach((System.out::println));
