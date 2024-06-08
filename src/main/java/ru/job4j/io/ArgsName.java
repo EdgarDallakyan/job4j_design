202a696f209ebd1fe4695d8f564010bcaeef45e8
edgar <edgar_dallakyan@mail.ru>
2024-06-08 19:08:44 +0300

    13. Именованные аргументы [#295518 #499880]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -17,33 +17,40 @@ public class ArgsName {
             values.put(s.substring(1, index), s.substring(index + 1));
         }
     }
-    public static ArgsName of(String[] args) {
+
+    private static void validate(String[] args) {
         if (args.length == 0) {
             throw new IllegalArgumentException("Arguments not passed to program");
         }
         for (String s : args) {
             if (!s.startsWith("-")) {
-                throw new IllegalArgumentException("Error: This argument '" + s + "' does not start with a '-' character");
+                throw new IllegalArgumentException(
+                        "Error: This argument '" + s + "' does not start with a '-' character");
             }
             if (!s.contains("=")) {
-                throw new IllegalArgumentException("Error: This argument '" + s + "' does not contain an equal sign");
+                throw new IllegalArgumentException(
+                        "Error: This argument '" + s + "' does not contain an equal sign");
             }
             if (s.startsWith("-=")) {
-                throw new IllegalArgumentException("Error: This argument '" + s + "' does not contain a key");
+                throw new IllegalArgumentException(
+                        "Error: This argument '" + s + "' does not contain a key");
             }
             if (s.indexOf('=') == s.length() - 1) {
-                throw new IllegalArgumentException("Error: This argument '" + s + "' does not contain a value");
+                throw new IllegalArgumentException(
+                        "Error: This argument '" + s + "' does not contain a value");
             }
         }
+    }
+
+    public static ArgsName of(String[] args) {
         ArgsName names = new ArgsName();
+        validate(args);
         names.parse(args);
         return names;
     }
-
     public static void main(String[] args) {
         ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
         System.out.println(jvm.get("Xmx"));
-
         ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
         System.out.println(zip.get("out"));
     }
