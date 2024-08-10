29cfb590f6ddff69116db4f5c570c1ddfbcf437b
edgar <edgar_dallakyan@mail.ru>
2024-08-10 17:03:08 +0300

    4.3. Транзакции. Уровни изолированности транзакций. [#504812]

---
+++ src/main/java/ru/job4j/sqldb/transaction.sql
@@ -0,0 +1,11 @@
+create table employees (
+    id serial primary key,,
+    name text,
+    department text,
+    salary int
+);
+
+insert into employees(name, departmentId, salary) values ('James',  'Police', 1000);
+insert into employees(name, departmentId, salary) values ('John',   'Ambulance', 400);
+insert into employees(name, departmentId, salary) values ('Michael',  'Firefighters', 2, 600);
+insert into employees(name, departmentId, salary) values ('Johnathon',  '911', 500);
