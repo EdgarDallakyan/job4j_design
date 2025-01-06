880595c19ce9216636b900d9f0a127e68cd9e791
edgar <edgar_dallakyan@mail.ru>
2025-01-06 13:26:13 +0300

    Бинарное дерево поиска. Удаление узла. [#504985]

--- src/main/java/ru/job4j/collection/binarytree/BinarySearchTree.java
+++ src/main/java/ru/job4j/collection/binarytree/BinarySearchTree.java
@@ -3,6 +3,7 @@ package ru.job4j.collection.binarytree;
 import java.util.*;
 
 public class BinarySearchTree<T extends Comparable<T>> {
+
     private Node root;
 
     public boolean put(T key) {
@@ -18,7 +19,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 
     private boolean put(Node node, T key) {
         boolean result = false;
-        int comparingResult = node.getText().compareTo(key.toString());
+        int comparingResult = node.key.compareTo(key);
         if (comparingResult < 0) {
             if (node.right == null) {
                 node.right = new Node(key);
@@ -47,7 +48,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
             if (node.getText().equals(key.toString())) {
                 result = node;
             } else {
-                int comparingResult = node.getText().compareTo(key.toString());
+                int comparingResult = node.key.compareTo(key);
                 if (comparingResult < 0) {
                     result = find(node.right, key);
                 } else {
@@ -58,11 +59,6 @@ public class BinarySearchTree<T extends Comparable<T>> {
             result = null;
         }
         return result;
-    }
-
-    public boolean remove(T key) {
-        /* Метод будет реализован в следующих уроках */
-        return false;
     }
 
     public List<T> inSymmetricalOrder() {
@@ -143,10 +139,86 @@ public class BinarySearchTree<T extends Comparable<T>> {
         return PrintTree.getTreeDisplay(root);
     }
 
+    public boolean remove(T key) {
+        boolean result = false;
+        if (Objects.nonNull(key) && Objects.nonNull(root)) {
+            result = remove(root, key);
+        }
+        return result;
+    }
+
+    private boolean remove(Node source, T key) {
+        boolean result = true;
+        Node current = source;
+        Node parent = source;
+        boolean isLeft = true;
+        while (result && !Objects.equals(current.key, key)) {
+            parent = current;
+            int cmp = key.compareTo(current.key);
+            if (cmp < 0) {
+                isLeft = true;
+                current = current.left;
+            } else if (cmp > 0) {
+                isLeft = false;
+                current = current.right;
+            }
+            if (Objects.isNull(current)) {
+                result = false;
+            }
+        }
+        if (result) {
+            if (Objects.isNull(current.left) && Objects.isNull(current.right)) {
+                swap(isLeft, source, parent, current, null);
+            } else if (Objects.nonNull(current.left) && Objects.isNull(current.right)) {
+                swap(isLeft, source, parent, current, current.left);
+            } else if (Objects.isNull(current.left)) {
+                swap(isLeft, source, parent, current, current.right);
+            } else {
+                Node heir = getHeir(current);
+                swap(isLeft, source, parent, current, heir);
+                heir.left = current.left;
+            }
+        }
+        if (!Objects.isNull(current)) {
+            current.key = null;
+            current.left = null;
+            current.right = null;
+            current = null;
+        }
+        return result;
+    }
+
+    private void swap(boolean isLeft, Node source, Node parent, Node current, Node equal) {
+        if (Objects.equals(current, source)) {
+            root = equal;
+        } else if (isLeft) {
+            parent.left = equal;
+        } else {
+            parent.right = equal;
+        }
+    }
+
+    private Node getHeir(Node delNode) {
+        Node nodeParent = delNode;
+        Node node = delNode;
+        Node current = delNode.right;
+        while (current != null) {
+            nodeParent = node;
+            node = current;
+            current = current.left;
+        }
+        if (node != delNode.right) {
+            nodeParent.left = node.right;
+            node.right = delNode.right;
+        }
+        return node;
+    }
+
     private class Node implements VisualNode {
         private T key;
         private Node left;
         private Node right;
+
         public Node(T key) {
             this.key = key;
         }
