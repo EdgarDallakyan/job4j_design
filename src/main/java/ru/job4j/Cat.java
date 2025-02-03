daa360ccde06989393d00a2f07e52775ecb32ee2
edgar <edgar_dallakyan@mail.ru>
2025-02-03 15:11:50 +0300

    3.2. Откат изменений [#504830]

---
+++ src/main/java/ru/job4j/Cat.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Cat {
+    public static void main(String[] args) {
+        System.out.println("мяу-мяу");
+        System.out.println("спит");
+    }
+}
---
+++ src/main/java/ru/job4j/Dog.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Dog {
+    public static void main(String[] args) {
+        System.out.println("Вуф-вуф");
+        System.out.println("Сидеть");
+    }
+}
